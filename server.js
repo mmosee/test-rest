2b0529a6263981c3a9b72d23ea14daada9fc6c3c
mmosee <nammosee@gmail.com>
2024-04-11 14:50:00 +0900

    server.js modify

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get("/", function (req, res) {
     return res.send("hello world")
 })
 
+app.post('/save', function(req, res) {
+    console.log('debug: /save');
+    return res.status(200).json({
+        "test":"test"
+    });
+});
+
+app.post('/validate', function(req, res) {
+    console.log('debug: /validate');
+    return res.status(200).json({});
+});
+
 app.listen(3000, function () {
     console.log("server listening on port 3000")
 })
